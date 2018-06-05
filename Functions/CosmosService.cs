0b974c2dcd87210b7e58842a302ba87a2a77fcc0
nzcoward <i.coward@gmail.com>
2018-06-05 16:43:01 +0100

    Jonos Silly Stuff

---
+++ Functions/CosmosService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Newtonsoft.Json;
+
+namespace BFYOC
+{
+    public class CosmosService
+    {
+        public async Task<Rating> CreateRatingFromDocument(dynamic data, IAsyncCollector<Rating> document)
+        {
+            // Set unique id
+            var id = Guid.NewGuid();
+
+            // Set Timestamp
+            var timestamp = DateTime.UtcNow;
+
+            var rating = new Rating
+            {
+                id = id,
+                userId = data.userId,
+                productId = data.productId,
+                timestamp = timestamp,
+                locationName = data.locationName,
+                rating = data.rating,
+                userNotes = data.userNotes
+            };
+
+            await document.AddAsync(rating);
+
+            return rating;
+        }
+    }
+}
