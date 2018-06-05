b0d090e4e5cd9c8b3bd981d3998f1025f4d5f579
Suresh <sureshbabu78@live.com>
2018-06-05 13:39:01 +0000

    updated rating

--- GetRattingHttpTrigger.cs
+++ GetRattingHttpTrigger.cs
@@ -5,24 +5,34 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Extensions.CosmosDB;
 using Newtonsoft.Json;
+using System;
 
 namespace BFYOC
 {
     public static class GetRattingHttpTrigger
     {
         [FunctionName("GetRattingHttpTrigger")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "ratings/{ratingId}")]HttpRequest req,
+             [CosmosDB(
+                databaseName: "Challenge2",
+                collectionName: "Ratings",
+                ConnectionStringSetting = "CosmosDBConnectionString",
+                Id = "{ratingId}")]dynamic rating, TraceWriter log)
         {
             log.Info("Request for Get Ratting Http Trigger.");
 
             string ratingId = req.Query["ratingId"];
 
-            if (ratingId == null) new BadRequestObjectResult("Please Enter RattingID");
+            if (ratingId == null) return new BadRequestObjectResult("Please Enter RattingID");
+                
 
-            var ratting = new Rating();
+           // var ratting = new Rating();
 
-            return (ActionResult)new OkObjectResult(ratting);
+            return (ActionResult)new OkObjectResult(rating);
         }
     }
+
 }
