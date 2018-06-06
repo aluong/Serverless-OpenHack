fe9afff9e3dce99da3b6fbda62fe94ea09fbc037
nzcoward <i.coward@gmail.com>
2018-06-06 09:29:35 +0100

    Added a get for ratings...

--- Functions/GetRatingsForUser.cs
+++ Functions/GetRatingsForUser.cs
@@ -1,20 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace BFYOC
 {
-    public static class GetRatings
+    public static class GetRatingsForUser
     {
-        [FunctionName("GetRatings")]
+        [FunctionName("GetRatingsForUser")]
         public static IActionResult Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "ratings")]HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "users/{userId:guid}/ratings")]HttpRequest req, 
             [CosmosDB("Challenge2", "Ratings",
                 ConnectionStringSetting = "CosmosDBConnectionString",
-                SqlQuery = "select * from Ratings")]
+                SqlQuery = "select * from Ratings r where r.userId = {userId}")]
                 IEnumerable<Rating> ratings,
             TraceWriter log
         )
