58f2559b77b581200601483c4d0e628779f38dcc
nzcoward <i.coward@gmail.com>
2018-06-06 16:42:26 +0100

    Sales event handler

---
+++ Functions/SalePlacedEventHandler.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+using Microsoft.Azure.WebJobs.Host;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Concurrent;
+using Microsoft.WindowsAzure.Storage;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Linq;
+using Microsoft.Azure.WebJobs.ServiceBus;
+using Microsoft.Azure.EventHubs;
+
+namespace BFYOC
+{
+    public static class SalesPlacedEventHandler
+    {
+
+        [FunctionName("SalesPlacedEventHandler")]
+        public static async Task Run(
+            [EventHubTrigger("32inside", Connection = "EventHubConnectionAppSetting")]string message,
+            TraceWriter log)
+        {
+            log.Info(message);
+        }
+    }
+}
